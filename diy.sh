1bf06ab5d7ac58ece2072e134e967f633d6c8ef1
mengmengniao <731729875@qq.com>
2025-10-02 01:52:21 +0800

    diy.sh

--- diy.sh
+++ diy.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# === Part 0: The Ultimate Go Build Fix ===
+# This addresses a common build error with Go 1.18+ packages like v2ray-plugin
+# It tells the Go compiler to ignore the lack of a version control system.
+sed -i 's/GO_PKG_TARGET_VARS:=/GO_PKG_TARGET_VARS:=\nexport GOFLAGS:=-buildvcs=false/' feeds/packages/lang/golang/golang.mk
+
 # === Part 1: Clone all source repositories ===
 # We only need to clone the main 'all-in-one' repositories
 git clone https://github.com/ophub/luci-app-amlogic --depth=1 clone/amlogic
@@ -8,7 +13,7 @@ git clone https://github.com/fw876/helloworld.git --depth=1 clone/helloworld
 git clone https://github.com/chenmoha/luci-app-turboacc.git --depth=1 clone/turboacc
 git clone https://github.com/sbwml/luci-app-mosdns.git --depth=1 clone/mosdns
 
-# === Part 2: Organize packages (The Thorough Fix) ===
+# === Part 2: Organize packages (The Thorough Method) ===
 # Clean any old versions that might conflict
 rm -rf feeds/luci/applications/luci-app-passwall
 
@@ -16,37 +21,28 @@ rm -rf feeds/luci/applications/luci-app-passwall
 cp -rf clone/amlogic/luci-app-amlogic feeds/luci/applications/
 cp -rf clone/passwall/luci-app-passwall feeds/luci/applications/
 cp -rf clone/turboacc/luci-app-turboacc feeds/luci/applications/
-# Copy LuCI apps from the 'all-in-one' repos
 cp -rf clone/helloworld/luci-app-ssr-plus feeds/luci/applications/
 cp -rf clone/mosdns/luci-app-mosdns feeds/luci/applications/
 
-# --- THIS IS THE ULTIMATE FIX ---
-# Instead of cherry-picking, copy ALL packages from the complex repositories.
-# This ensures all dependencies like v2ray-plugin, dns2socks-rust, v2dat, etc., are included.
-
-# Copy all packages from helloworld repository
+# Copy ALL packages from the complex repositories to ensure all dependencies are met.
 # Use a loop to ensure we only copy directories (which are packages)
 for dir in clone/helloworld/*; do
   if [ -d "$dir" ]; then
     package_name=$(basename "$dir")
-    # We already copied luci-app-ssr-plus, so skip it here
     if [ "$package_name" != "luci-app-ssr-plus" ]; then
       cp -rf "$dir" package/
     fi
   fi
 done
 
-# Copy all packages from luci-app-mosdns repository (it includes mosdns, v2dat, etc.)
 for dir in clone/mosdns/*; do
   if [ -d "$dir" ]; then
     package_name=$(basename "$dir")
-    # We already copied luci-app-mosdns, so skip it here
     if [ "$package_name" != "luci-app-mosdns" ]; then
       cp -rf "$dir" package/
     fi
   fi
 done
-# --- END OF FIX ---
 
 # === Part 3: Clean up ===
 rm -rf clone
