10e76bb25a9fbe4d443fae0f0da25367c28d87b1
mengmengniao <731729875@qq.com>
2025-10-05 13:00:03 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -1,40 +1,12 @@
 #!/bin/bash
 
-# === Part 0: The Go Build Fix (harmless to keep) ===
-sed -i 's/GO_PKG_TARGET_VARS:=/GO_PKG_TARGET_VARS:=\nexport GOFLAGS:=-buildvcs=false/' feeds/packages/lang/golang/golang.mk
-
-# === Part 1: Clone all source repositories ===
+# Add packages
 git clone https://github.com/ophub/luci-app-amlogic --depth=1 clone/amlogic
 git clone https://github.com/xiaorouji/openwrt-passwall --depth=1 clone/passwall
-git clone https://github.com/fw86/helloworld.git --depth=1 clone/helloworld
-git clone https://github.com/chenmoha/luci-app-turboacc.git --depth=1 clone/turboacc
-git clone https://github.com/sbwml/luci-app-mosdns.git --depth=1 clone/mosdns
 
-# === Part 2: Organize packages (The Thorough Method) ===
+# Update packages
 rm -rf feeds/luci/applications/luci-app-passwall
-cp -rf clone/amlogic/luci-app-amlogic feeds/luci/applications/
-cp -rf clone/passwall/luci-app-passwall feeds/luci/applications/
-cp -rf clone/turboacc/luci-app-turboacc feeds/luci/applications/
-cp -rf clone/helloworld/luci-app-ssr-plus feeds/luci/applications/
-cp -rf clone/mosdns/luci-app-mosdns feeds/luci/applications/
+cp -rf clone/amlogic/luci-app-amlogic clone/passwall/luci-app-passwall feeds/luci/applications/
 
-for dir in clone/helloworld/*; do
-  if [ -d "$dir" ]; then
-    package_name=$(basename "$dir")
-    if [ "$package_name" != "luci-app-ssr-plus" ]; then
-      cp -rf "$dir" package/
-    fi
-  fi
-done
-
-for dir in clone/mosdns/*; do
-  if [ -d "$dir" ]; then
-    package_name=$(basename "$dir")
-    if [ "$package_name" != "luci-app-mosdns" ]; then
-      cp -rf "$dir" package/
-    fi
-  fi
-done
-
-# === Part 3: Clean up ===
+# Clean packages
 rm -rf clone
