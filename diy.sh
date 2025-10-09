efcf4d3905d7c5fd6d784bfea5f89cc04e7a743f
mengmengniao <731729875@qq.com>
2025-10-09 12:17:52 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -12,15 +12,15 @@ git clone https://github.com/sbwml/packages_lang_golang -b 22.x --single-branch 
 git clone https://github.com/sbwml/v2ray-geodata --depth=1 clone/v2ray-geodata
 git clone https://github.com/xiaorouji/openwrt-passwall --depth=1 clone/passwall
 # 克隆 helloworld 仓库luci-app-ssr-plus
-git clone https://github.com/fw876/helloworld --depth=1 package/helloworld-temp
+# git clone https://github.com/fw876/helloworld --depth=1 package/helloworld-temp
 
 # Adjust packages
 rm -rf feeds/luci/applications/luci-app-passwall feeds/packages/lang/golang feeds/packages/net/mosdns feeds/packages/net/v2ray-geodata
 cp -rf clone/amlogic/luci-app-amlogic clone/mosdns/luci-app-mosdns clone/passwall/luci-app-passwall feeds/luci/applications/
 cp -rf clone/golang feeds/packages/lang/
 cp -rf clone/mosdns/mosdns clone/mosdns/v2dat clone/my-pkg/haproxy clone/v2ray-geodata feeds/packages/net/
-cp -rf clone/my-pkg/luci-app-mosdns clone/my-pkg/luci-app-passwall feeds/luci/applications/
-cp -rf package/helloworld-temp/luci-app-ssr-plus feeds/luci/applications/
+cp -rf clone/my-pkg/luci-app-mosdns  feeds/luci/applications/
+# cp -rf package/helloworld-temp/luci-app-ssr-plus feeds/luci/applications/
 sed -i '/docker-compose/d' feeds/luci/applications/luci-app-dockerman/Makefile
 
 # Clean packages
